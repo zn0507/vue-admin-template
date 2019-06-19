04d81b026ada259ce6739d41771ce1b433e470fa
zn <619232906@qq.com>
2019-06-19 23:29:43 +0800

    import echarts

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -3,14 +3,51 @@
     <div class="dashboard-text">name:{{ name }}</div>
     <div class="dashboard-text">roles:<span v-for="role in roles" :key="role">{{ role }}</span></div>
     <div>
-      当日访问量，分时访问量，累计访问量，在线平均时长。最小时长，地域图
+      <el-row :gutter="10">
+        <el-col :span="4">
+          <el-card shadow="hover">
+            分时访问量
+          </el-card>
+        </el-col>
+        <el-col :span="4">
+          <el-card shadow="hover">
+            当日访问量
+          </el-card>
+        </el-col>
+        <el-col :span="4">
+          <el-card shadow="hover">
+            累计访问量
+          </el-card>
+        </el-col>
+        <el-col :span="4">
+          <el-card shadow="hover">
+            在线平均时长
+          </el-card>
+        </el-col>
+        <el-col :span="4">
+          <el-card shadow="hover">
+            地域图
+          </el-card>
+        </el-col>
+        <el-col :span="4">
+          <el-card shadow="hover">
+            累计访问量
+          </el-card>
+        </el-col>
+      </el-row>
+      <el-row>
+        <div id="myChart" :style="{width: '300px', height: '300px'}"/>
+      </el-row>
     </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
-
+const echarts = require('echarts/lib/echarts')
+require('echarts/lib/chart/bar')
+require('echarts/lib/component/tooltip')
+require('echarts/lib/component/title')
 export default {
   name: 'Dashboard',
   computed: {
@@ -18,6 +55,27 @@ export default {
       'name',
       'roles'
     ])
+  },
+  mounted() {
+    this.drawLine()
+  },
+  methods: {
+    drawLine() {
+      const myChart = echarts.init(document.getElementById('myChart'))
+      myChart.setOption({
+        title: { text: 'ECharts 入门示例' },
+        tooltip: {},
+        xAxis: {
+          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+        },
+        yAxis: {},
+        series: [{
+          name: '销量',
+          type: 'bar',
+          data: [5, 20, 36, 10, 10, 20]
+        }]
+      })
+    }
   }
 }
 </script>
